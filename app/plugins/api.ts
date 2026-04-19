4cd9e2d49e433bc93b24f8a4dcf7e5434900521e
Songyot888 <dog1235you@gmail.com>
2026-04-20 00:08:13 +0700

    push

--- app/plugins/api.ts
+++ app/plugins/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default defineNuxtPlugin(() => {
   const api = axios.create({
-    baseURL: "http://localhost:8080", // API base
+    baseURL: "https://url-shortener-api-rgj5.onrender.com", // API base
     timeout: 5000,
   });
 
